9b4d0ed5e723290cae5f6f037a43d9ecb999018e
Dezekah Fuller <dezekahf@gmail.com>
2025-05-12 19:46:51 -0400

    menubar changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3x1 font-bold text-gray-800 ab-2">Final Exam 343</h1>
+      <h1 className="text-3x1 font-bold text-gray-800 mb-2">Final Exam 343</h1>
       <h2 className="text-3x1 font-semibold text-gray-700 mb-1">Fuller, Dezekah</h2>
       <h3 className="text-x1 text-gray-600">Spring 2025</h3>
 
