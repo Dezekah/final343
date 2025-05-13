4a62bdb67a5713b3ffc1bddf5d4559c67d802e8d
Dezekah Fuller <dezekahf@gmail.com>
2025-05-12 22:08:35 -0400

    new changes

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -8,7 +8,7 @@ export default function Menubar() {
             <Link href='/'>Home</Link>
           </li>
           <li>
-            <a href='https://exam1-lyart.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
+            <a href='https://expense-omega-seven.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
           </li>
           <li>
             <a href='https://exam2-pi.vercel.app/' target='_blank'>Midterm Part II Site</a>
